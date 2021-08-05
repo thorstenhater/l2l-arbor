ca5d61bb931bafdd70f828206d47cfadd2f9f4e7
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2021-08-05 15:21:06 +0200

    update

--- model.py
+++ model.py
@@ -8,21 +8,21 @@ from l2l.utils.experiment import Experiment
 from l2l.optimizers.evolution import GeneticAlgorithmOptimizer, GeneticAlgorithmParameters
 from l2l.optimizees.arbor.SC import ArbSCOptimizee
 
-fit, swc, ref = list(map(expand, sys.argv[1:]))
-name = 'ARBOR-FUN'
-results_folder = '../results'
+fit, swc, ref   = list(map(expand, sys.argv[1:]))
+name            = 'ARBOR-FUN'
+results_folder  = '../results'
 trajectory_name = 'ARBOR'
-experiment = Experiment(results_folder)
+experiment      = Experiment(results_folder)
 traj, _ = experiment.prepare_experiment(trajectory_name=trajectory_name,
                                         name=name,
-                                        jube_parameter={})
+                                        jube_parameter={"exec": "srun -n 1 -c 8 --exclusive python"})
 # Innerloop simulator
 optimizee = ArbSCOptimizee(traj, fit, swc, ref)
 # Outerloop optimizer initialization
-parameters = GeneticAlgorithmParameters(seed=0, popsize=10,
+parameters = GeneticAlgorithmParameters(seed=0, popsize=100,
                                         CXPB=0.5, MUTPB=0.3, NGEN=10,
                                         indpb=0.02,
-                                        tournsize=15,
+                                        tournsize=100,
                                         matepar=0.5,
                                         mutpar=1)
 optimizer = GeneticAlgorithmOptimizer(traj,
